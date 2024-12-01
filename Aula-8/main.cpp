2785a1d30d47fbcbfe12274b470caea6fd3315dc
RodboN <rodrigoboncardoso@yahoo.com>
2024-12-01 16:50:58 -0300

    + exemplo video aula

--- Aula-8/main.cpp
+++ Aula-8/main.cpp
@@ -26,5 +26,23 @@ int main() {
     u64 prod = produto(num_a, num_b);
     cout << "Produto: " << prod << endl;
 
+    //EXEMPLO VIDEO AULA
+    Uint a[3];
+    *(*(a + 0) + 0) = 0;
+    a[1][1] = 1;
+    for(int i = 0; i < 3; ++i){
+        for(int j = 0; j < 3; ++j){
+            *(*(a + i) + j) = 0;
+        }
+    }
+    for(int i = 0; i < 3; ++i){
+        for(int j = 0; j < 3; ++j){
+            std::cout << *(*(a + i) + j) << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+ 
+    std::cout << std::endl;
     return 0;
 }
